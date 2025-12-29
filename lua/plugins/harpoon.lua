f65779aecbba655003f5246a0b7fb3788d486963
TheColGateMann4 <TheColGateMann4@gmail.com>
2025-12-29 11:00:54 +0100

    Improved Harpoon Workflow

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,26 +1,32 @@
 return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    lazy = false,
-    config = function()
-        local harpoon = require("harpoon")
-        harpoon.setup()
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	lazy = false,
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon.setup({
+			global_settings = {
+				save_on_toggle = false,
+				save_on_change = true,
+				excluded_filetypes = { "harpoon" },
+			},
+		})
 
-        local list = harpoon:list()
-        local ui = harpoon.ui
+		local list = harpoon:list()
+		local ui = harpoon.ui
 
-        vim.keymap.set("n", "<M-a>", function() list:add() end)
-        vim.keymap.set("n", "<M-q>", function() ui:toggle_quick_menu(list) end)
-        vim.keymap.set("n", "<M-1>", function() list:select(1) end)
-        vim.keymap.set("n", "<M-2>", function() list:select(2) end)
-        vim.keymap.set("n", "<M-3>", function() list:select(3) end)
-        vim.keymap.set("n", "<M-4>", function() list:select(4) end)
-        vim.keymap.set("n", "<M-5>", function() list:select(5) end)
-        vim.keymap.set("n", "<M-6>", function() list:select(6) end)
-        vim.keymap.set("n", "<M-7>", function() list:select(7) end)
-        vim.keymap.set("n", "<M-8>", function() list:select(8) end)
-        vim.keymap.set("n", "<M-9>", function() list:select(9) end)
-        vim.keymap.set("n", "<M-0>", function() list:select(10) end)
-    end,
+		vim.keymap.set("n", "<M-a>", function() list:add() end)
+		vim.keymap.set("n", "<M-q>", function() ui:toggle_quick_menu(list) end)
+		vim.keymap.set("n", "<M-1>", function() list:select(1) end)
+		vim.keymap.set("n", "<M-2>", function() list:select(2) end)
+		vim.keymap.set("n", "<M-3>", function() list:select(3) end)
+		vim.keymap.set("n", "<M-4>", function() list:select(4) end)
+		vim.keymap.set("n", "<M-5>", function() list:select(5) end)
+		vim.keymap.set("n", "<M-6>", function() list:select(6) end)
+		vim.keymap.set("n", "<M-7>", function() list:select(7) end)
+		vim.keymap.set("n", "<M-8>", function() list:select(8) end)
+		vim.keymap.set("n", "<M-9>", function() list:select(9) end)
+		vim.keymap.set("n", "<M-0>", function() list:select(10) end)
+	end,
 }
