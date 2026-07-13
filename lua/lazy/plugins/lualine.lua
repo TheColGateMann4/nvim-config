83f325a96d3120ad8d6ae2ad2d1cb1091c89c797
ColGate <thecolgatemann4@gmail.com>
2026-07-13 19:36:35 +0200

    Removed catppucin from lualine

--- lua/lazy/plugins/lualine.lua
+++ lua/lazy/plugins/lualine.lua
@@ -1,11 +1,9 @@
 return 
 {
 	"nvim-lualine/lualine.nvim",
-	dependencies = {"catppuccin/nvim"},
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "catppuccin",
 				globalstatus = true,
 				section_separators = { left = "", right = "" },
 				component_separators = { left = " | ", right = " | " },
